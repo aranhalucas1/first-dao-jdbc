6848059b816c120b7c01263f7b45a0b5aa01cf06
Lucas Aranha <aranhalucas@pop-os.localdomain>
2020-05-05 12:42:04 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,6 +32,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Renato Aranha", "renato@gmail.com", new Date(), 6000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id: " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Maria de Jesus");
+		sellerDao.update(seller);
+		System.out.println("Updated!");
 	}
 
 }
